707eb51515c5cd5cace7132a6d4daf4642714e85
fdeluna <fernando.deluna.romero@gmail.com>
2018-07-08 01:17:21 +0200

    Camera max/min size, camera boundaries, clamp tank movement to viewport

--- Assets/_Completed-Assets/Scripts/Camera/CameraControl.cs
+++ Assets/_Completed-Assets/Scripts/Camera/CameraControl.cs
@@ -1,50 +1,69 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace Complete
 {
     public class CameraControl : MonoBehaviour
     {
+
         public float m_DampTime = 0.2f;                 // Approximate time for the camera to refocus.
         public float m_ScreenEdgeBuffer = 4f;           // Space between the top/bottom most target and the screen edge.
+        [Header("Camera Zoom")]
+        public float m_MaxSize = 17f;                   // The biggest orthographic size the camera can be.
         public float m_MinSize = 6.5f;                  // The smallest orthographic size the camera can be.
         [HideInInspector] public Transform[] m_Targets; // All the targets the camera needs to encompass.
+        [Header("Boundaires")]
+        public float m_TopBoundaire = 0.5f;             // Top Camera viewport boundary
+        public float m_BotBoundaire = 0.5f;             // Bottom Camera viewport boundary
+        public float m_RightBoundaire = 0.5f;           // Right Camera viewport boundary
+        public float m_LeftBoundaire = 0.5f;            // Left Camera viewport boundary
 
-
+        private Vector3 _topBoundry;                    // Top boundry World position for Camera boundaries
+        private Vector3 _bottomBoundry;                 // Bottom boundry World position for Camera boundaries
+        private Vector3 _leftBoundry;                   // Left boundry X World position for Camera boundaries
+        private Vector3 _rightBoundry;                  // Right boundry  World position for Camera boundaries
         private Camera m_Camera;                        // Used for referencing the camera.
         private float m_ZoomSpeed;                      // Reference speed for the smooth damping of the orthographic size.
         private Vector3 m_MoveVelocity;                 // Reference velocity for the smooth damping of the position.
-        private Vector3 m_DesiredPosition;              // The position the camera is moving towards.
+        private Vector3 m_DesiredPosition;              // The position the camera is moving towards.              
 
-
-        private void Awake ()
+        private void Awake()
         {
-            m_Camera = GetComponentInChildren<Camera> ();
+            m_Camera = GetComponentInChildren<Camera>();
+            SetWorldBoundaires();
         }
 
-
-        private void FixedUpdate ()
+        private void FixedUpdate()
         {
             // Move the camera towards a desired position.
-            Move ();
+            Move();
 
             // Change the size of the camera based.
-            Zoom ();
+            Zoom();
         }
 
-
-        private void Move ()
+        private void Move()
         {
             // Find the average position of the targets.
-            FindAveragePosition ();
+            FindAveragePosition();
 
-            // Smoothly transition to that position.
-            transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+            // Smoothly transition to that position
+            Vector3 movePosition = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+
+            transform.position = movePosition;
+                        
+            // Apply Boundaires
+            Vector3 applyBoundaires = transform.position;
+            applyBoundaires.x = Mathf.Clamp(applyBoundaires.x, _leftBoundry.x, _rightBoundry.x);
+            applyBoundaires.z = Mathf.Clamp(applyBoundaires.z, _bottomBoundry.z, _topBoundry.z);
+
+            transform.position = applyBoundaires;
         }
 
 
-        private void FindAveragePosition ()
+        private void FindAveragePosition()
         {
-            Vector3 averagePos = new Vector3 ();
+            Vector3 averagePos = new Vector3();
             int numTargets = 0;
 
             // Go through all the targets and add their positions together.
@@ -71,15 +90,16 @@ namespace Complete
         }
 
 
-        private void Zoom ()
+        private void Zoom()
         {
             // Find the required size based on the desired position and smoothly transition to that size.
             float requiredSize = FindRequiredSize();
-            m_Camera.orthographicSize = Mathf.SmoothDamp (m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+
+            m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
         }
 
 
-        private float FindRequiredSize ()
+        private float FindRequiredSize()
         {
             // Find the position the camera rig is moving towards in its local space.
             Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
@@ -110,23 +130,89 @@ namespace Complete
             // Add the edge buffer to the size.
             size += m_ScreenEdgeBuffer;
 
-            // Make sure the camera's size isn't below the minimum.
-            size = Mathf.Max (size, m_MinSize);
+            // Make sure the camera's size is between Max and Min.            
+            size = Mathf.Clamp(size, m_MinSize, m_MaxSize);
 
             return size;
         }
 
 
-        public void SetStartPositionAndSize ()
+        public void SetStartPositionAndSize()
         {
             // Find the desired position.
-            FindAveragePosition ();
+            FindAveragePosition();
 
             // Set the camera's position to the desired position without damping.
             transform.position = m_DesiredPosition;
 
             // Find and set the required size of the camera.
-            m_Camera.orthographicSize = FindRequiredSize ();
+            m_Camera.orthographicSize = FindRequiredSize();
+        }
+
+        private void SetWorldBoundaires()
+        {            
+            RaycastHit hitTop;
+            RaycastHit hitBottom;
+            RaycastHit hitRight;
+            RaycastHit hitLeft;
+
+            Physics.Raycast(m_Camera.ViewportPointToRay(new Vector3(0.5f, m_TopBoundaire, Vector3.Distance(transform.position, m_Camera.transform.position))), out hitTop);
+            Physics.Raycast(m_Camera.ViewportPointToRay(new Vector3(0.5f, m_BotBoundaire, Vector3.Distance(transform.position, m_Camera.transform.position))), out hitBottom);
+            Physics.Raycast(m_Camera.ViewportPointToRay(new Vector3(m_RightBoundaire, 0.5f, Vector3.Distance(transform.position, m_Camera.transform.position))), out hitRight);
+            Physics.Raycast(m_Camera.ViewportPointToRay(new Vector3(m_LeftBoundaire, 0.5f, Vector3.Distance(transform.position, m_Camera.transform.position))), out hitLeft);
+
+            _topBoundry = hitTop.point;
+            _bottomBoundry = hitBottom.point;
+            _rightBoundry = hitRight.point;
+            _leftBoundry = hitLeft.point;                          
+        }
+
+        void OnDrawGizmos()
+        {
+            // boundaries Gizmos                                           
+            if (!EditorApplication.isPlaying)
+            {
+                m_Camera = GetComponentInChildren<Camera>();
+                SetWorldBoundaires();
+            }
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(_topBoundry, 2);
+            Gizmos.DrawSphere(_bottomBoundry, 2);
+            Gizmos.DrawSphere(_rightBoundry, 2);
+            Gizmos.DrawSphere(_leftBoundry, 2);
+ 
+            // Zoom Gizmos            
+            // Max Camera size
+            Gizmos.color = Color.red;
+            float maxVerticalHeightSeen = m_MinSize * 2.0f;
+            DrawWireCube(m_Camera.transform.position, transform.rotation, new Vector3((maxVerticalHeightSeen * m_Camera.aspect), maxVerticalHeightSeen, 0));
+
+            Gizmos.color = Color.magenta;
+            // Min Camera size
+            float minVerticalHeightSeen = m_MaxSize * 2.0f;
+            DrawWireCube(m_Camera.transform.position, transform.rotation, new Vector3((minVerticalHeightSeen * m_Camera.aspect), minVerticalHeightSeen, 0));
+
+            // Current Camera size
+            Gizmos.color = Color.green;
+            float currentVerticalHeightSeen = m_Camera.orthographicSize * 2.0f;
+            DrawWireCube(m_Camera.transform.position, transform.rotation, new Vector3((currentVerticalHeightSeen * m_Camera.aspect), currentVerticalHeightSeen, 0));
+
+        }
+
+
+        // DrawWireCube rotate
+        // https://answers.unity.com/questions/894217/confusion-with-gizmo-matrix-when-using-gizmo-cube.html
+        public static void DrawWireCube(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            Matrix4x4 cubeTransform = Matrix4x4.TRS(position, rotation, scale);
+            Matrix4x4 oldGizmosMatrix = Gizmos.matrix;
+
+            Gizmos.matrix *= cubeTransform;
+
+            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+
+            Gizmos.matrix = oldGizmosMatrix;
         }
     }
 }
--- Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankMovement.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 namespace Complete
 {
     public class TankMovement : MonoBehaviour
     {
+        public enum PlayerType { Player, AI };
+
         public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
         public float m_Speed = 12f;                 // How fast the tank moves forward and back.
         public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
         public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
         public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
         public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
-		public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
-
+        public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
+        public PlayerType m_PlayerType;
+        
         private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
         private string m_TurnAxisName;              // The name of the input axis for turning.
         private Rigidbody m_Rigidbody;              // Reference used to move the tank.
@@ -20,13 +24,13 @@ namespace Complete
         private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
         private ParticleSystem[] m_particleSystems; // References to all the particles systems used by the Tanks
 
-        private void Awake ()
+        private void Awake()
         {
-            m_Rigidbody = GetComponent<Rigidbody> ();
+            m_Rigidbody = GetComponent<Rigidbody>();
         }
 
 
-        private void OnEnable ()
+        private void OnEnable()
         {
             // When the tank is turned on, make sure it's not kinematic.
             m_Rigidbody.isKinematic = false;
@@ -42,56 +46,56 @@ namespace Complete
             for (int i = 0; i < m_particleSystems.Length; ++i)
             {
                 m_particleSystems[i].Play();
-            }
+            }            
         }
 
 
-        private void OnDisable ()
+        private void OnDisable()
         {
             // When the tank is turned off, set it to kinematic so it stops moving.
             m_Rigidbody.isKinematic = true;
 
             // Stop all particle system so it "reset" it's position to the actual one instead of thinking we moved when spawning
-            for(int i = 0; i < m_particleSystems.Length; ++i)
+            for (int i = 0; i < m_particleSystems.Length; ++i)
             {
                 m_particleSystems[i].Stop();
             }
         }
 
 
-        private void Start ()
+        private void Start()
         {
             // The axes names are based on player number.
             m_MovementAxisName = "Vertical" + m_PlayerNumber;
             m_TurnAxisName = "Horizontal" + m_PlayerNumber;
 
             // Store the original pitch of the audio source.
-            m_OriginalPitch = m_MovementAudio.pitch;
+            m_OriginalPitch = m_MovementAudio.pitch;            
         }
 
 
-        private void Update ()
+        private void Update()
         {
             // Store the value of both input axes.
-            m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
-            m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
+            m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+            m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
 
-            EngineAudio ();
+            EngineAudio();
         }
 
 
-        private void EngineAudio ()
+        private void EngineAudio()
         {
             // If there is no input (the tank is stationary)...
-            if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
+            if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f)
             {
                 // ... and if the audio source is currently playing the driving clip...
                 if (m_MovementAudio.clip == m_EngineDriving)
                 {
                     // ... change the clip to idling and play it.
                     m_MovementAudio.clip = m_EngineIdling;
-                    m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                    m_MovementAudio.Play ();
+                    m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                    m_MovementAudio.Play();
                 }
             }
             else
@@ -108,34 +112,42 @@ namespace Complete
         }
 
 
-        private void FixedUpdate ()
+        private void FixedUpdate()
         {
             // Adjust the rigidbodies position and orientation in FixedUpdate.
-            Move ();
-            Turn ();
+            Move();
+            Turn();
         }
 
 
-        private void Move ()
+        private void Move()
         {
             // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
             Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
 
             // Apply this movement to the rigidbody's position.
-            m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+            Vector3 desiredPosition = m_Rigidbody.position + movement;
+
+            // Clamp movement to viewport
+            Vector3 viewPosition = Camera.main.WorldToViewportPoint(desiredPosition);
+            viewPosition.x = Mathf.Clamp(viewPosition.x, 0.05f, 0.95f);
+            viewPosition.y = Mathf.Clamp(viewPosition.y, 0.05f, 0.95f);
+            desiredPosition = Camera.main.ViewportToWorldPoint(viewPosition);
+
+            m_Rigidbody.MovePosition(desiredPosition);
         }
 
 
-        private void Turn ()
+        private void Turn()
         {
             // Determine the number of degrees to be turned based on the input, speed and time between frames.
             float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
 
             // Make this into a rotation in the y axis.
-            Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
 
             // Apply this rotation to the rigidbody's rotation.
-            m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+            m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
         }
     }
 }
